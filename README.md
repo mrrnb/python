f79e60d2c1c25dfaee18258c65018272f75aaa74
osatmnzn <osatmnzn@vip.qq.com>
2015-11-23 10:03:47 +0800

    clean up

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # python
+
+这个project是在 http://www.the5fire.com/python-practice-1.html 网站上的一个练习项目：及时标记
+
+但是练习并没有完成
+
+暂时不做更进
--- the5fire/filter.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-
-self.addFilter(r'\*(.+?)\*', 'emphasis')
-self.addFilter(r'(http://[\.a-z0-9A-Z/]+)', 'url')
-self.addFilter(r'([\.a-zA-Z]+@[\.a-zA-Z]+[a-zA-Z]+)','mail')
